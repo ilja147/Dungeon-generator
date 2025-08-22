325ce76a2010e9a3107e19dd63efd646412a700a
Kvad <ilja14741@gmail.com>
2025-08-22 22:30:57 +0400

    реализовал CellularAutomata

--- algorithms.cpp
+++ algorithms.cpp
@@ -8,8 +8,8 @@ void Algorithms::DrunkardWalk(std::vector<std::vector<Tile>> &map, int steps)
     static std::mt19937 gen(rd());
     int width = map.size();
     int height = map[0].size();
-    std::uniform_int_distribution <int> distx(1,width-2);
-    std::uniform_int_distribution <int> disty(1,height - 2);
+    std::uniform_int_distribution <int> distx(1,width - 1);
+    std::uniform_int_distribution <int> disty(1,height - 1);
     std::uniform_int_distribution <int> dir(0,3);
     int ax = distx(gen);
     int ay = disty(gen);
@@ -37,4 +37,50 @@ void Algorithms::DrunkardWalk(std::vector<std::vector<Tile>> &map, int steps)
             break;
         }
     }
+}
+void Algorithms::CellularAutomata(std::vector<std::vector<Tile>> &map)
+{
+    std::vector<std::vector<Tile>> map2 = map;
+    for(int i = 0; i < map2.size(); i++)
+    {
+        for(int j = 0; j < map2[0].size(); j++)
+        {
+            int liveNeighbors = 0;
+            for(int dx = -1; dx <= 1; dx++)
+            {
+                for(int dy = -1; dy <= 1; dy++)
+                {
+                    if(dx == 0 && dy == 0)
+                    {
+                        continue;
+                    }
+                    int nx = i + dx;
+                    int ny = j + dy;
+                    if(nx < 0 || nx >= map2.size() || ny < 0 || ny >= map2[0].size())
+                        liveNeighbors++;
+                    else if(map2[nx][ny].type == TileType::WALL)
+                        liveNeighbors++;
+                }
+            }
+            if(map2[i][j].type == TileType::WALL)
+            {
+                if(liveNeighbors >= 4)
+                    map2[i][j].type = TileType::WALL;
+                else
+                    map2[i][j].type = TileType::FLOOR;
+            }
+            else
+            {
+                if(liveNeighbors >= 5)
+                    map2[i][j].type = TileType::WALL;
+                else
+                    map2[i][j].type = TileType::FLOOR;
+            }
+        }
+    }
+    map = map2;
+}
+void Algorithms::RoomCarving(std::vector<std::vector<Tile>> &map)
+{
+
 }
